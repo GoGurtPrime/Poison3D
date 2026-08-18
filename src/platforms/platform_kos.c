07bffe02b23df9c0793de1b8c7afe89dd4900537
Colin <colinalaws@gmail.com>
2026-08-18 00:23:47 -0400

    Press start to quit on dreamcast builds to make using dcload ip easier.

--- src/platforms/platform_kos.c
+++ src/platforms/platform_kos.c
@@ -5,17 +5,33 @@
 #include <GL/glu.h>
 #include <GL/glext.h>
 
+Platform *platform;
+
+static void on_press_start(uint8_t address, uint32_t buttons) {
+    (void)address;
+    (void)buttons;
+    // Shut down.
+    platform->running = false;
+}
+
 bool platform_init(Platform* p, const char* title, int w, int h) 
 {
+    platform = p;
+
     glKosInit();
 
     p->running = true;
+
+    /* Subscribe a callback to be notified whenever the start button is pressed
+    on any connected controller. */
+    cont_btn_callback(0, CONT_START, on_press_start);
 
     return true;
 }
 
 void platform_pump(Platform* p) 
 {
+     
 }
 
 void platform_swap(Platform* p) 
